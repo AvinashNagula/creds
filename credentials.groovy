6180353ba6715bcdb8f0c648365d925f91597f09
madhu <madhuri.bairi55@gmail.com>
2024-08-07 19:14:24 +0530

    jenkinsfile

--- credentials.groovy
+++ credentials.groovy
@@ -1,7 +1,8 @@
 return [
     CERTS: [
         [NAME: 'CERT_1', FILE: 'cert1.crt', ID: 'secid1'],
-        [NAME: 'CERT_2', FILE: 'cert2.crt', ID: 'secid2']
+        [NAME: 'CERT_2', FILE: 'cert2.crt', ID: 'secid2'],
+        [NAME: 'secret', FILE: 'secret', ID: 'secret']
         // Add more certificates as needed
     ]
 ]
